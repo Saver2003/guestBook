0abd4b25e7d3e392662917b17e566cb3576624b0
alex <saver2003@gmail.com>
2021-12-28 15:01:44 +0600

    created guestBook model and fixtures

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from webapp.models import GuestBook
 
 # Register your models here.
+
+class GuestBookAdmin(admin.ModelAdmin):
+    list_display = ['id', 'author_name', 'status', 'created_at']
+    list_filter = ['id', 'author_name', 'status', 'created_at']
+    search_fields = ['author_name']
+    fields = ['author_name', 'author_email', 'post_text', 'status']
+
+admin.site.register(GuestBook, GuestBookAdmin)
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class GuestBook(models.Model):
+    STATUS_CHOISES = [
+        ('active', 'Активно'),
+        ('blocked', 'Заблокировано')
+    ]
+    author_name = models.CharField(max_length=100, null=False, blank=False, verbose_name='Имя автора')
+    author_email = models.EmailField(max_length=254, null=False, blank=False, verbose_name='Почта автора')
+    post_text = models.TextField(max_length=3000, null=False, blank=False, verbose_name='Текст записи')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Время изменения')
+    status = models.TextField(max_length=100, null=False, blank=False, choices=STATUS_CHOISES, default='active', verbose_name='Статус')
+
+    def __str__(self) -> str:
+        return "{}. {} {} {}".format(self.pk, self.author_name, self.status, self.created_at)
